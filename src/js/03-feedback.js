54ca4eb6f8dbac273fd76b8ef2114ff8c2bfe33d
Yevhenii2022 <3starosta3@gmail.com>
2023-03-11 17:00:51 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import _throttle from 'lodash.throttle';
 
+const FORM_STORAGE_KEY = 'feedback-form-state';
+
+const formEl = document.querySelector('.feedback-form');
+
+saveFormData();
+
+formEl.addEventListener('input', _throttle(handleFormElInput, 500));
+
+function handleFormElInput(event) {
+  let savedData = localStorage.getItem(FORM_STORAGE_KEY);
+  savedData = savedData ? JSON.parse(savedData) : {};
+  savedData[event.target.name] = event.target.value;
+  localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(savedData));
+}
+
+function saveFormData() {
+  let persistedData = JSON.parse(localStorage.getItem(FORM_STORAGE_KEY));
+
+  if (persistedData) {
+    Object.entries(persistedData).forEach(([name, value]) => {
+      formEl.elements[name].value = value;
+    });
+  }
+}
+
+formEl.addEventListener('submit', handleFormElSubmit);
+
+function handleFormElSubmit(event) {
+  event.preventDefault();
+  formEl.reset();
+  let savedData = localStorage.getItem(FORM_STORAGE_KEY);
+  savedData = savedData ? JSON.parse(savedData) : {};
+  console.log(savedData);
+  localStorage.removeItem(FORM_STORAGE_KEY);
+}
